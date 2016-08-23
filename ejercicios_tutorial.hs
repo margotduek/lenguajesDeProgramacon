63e4b007102e23181d16ead63c4fb737f954b3c0
margotduek <margotduek@gmail.com>
2016-08-23 09:58:34 -0500

    :rocket: taera 2

--- ejercicios_tutorial.hs
+++ ejercicios_tutorial.hs
@@ -1,5 +1,8 @@
 -- Se pueden concatenar listas con ++
+-- Para concatenar caracteres a una palabra 'H':"ola"
 -- let es para dejar algo declarado en el programa como let a = 0
+-- Para buscar algo por su indice se usa !! por ejemplo   "Margot" !! 3  me regresa g  
+
 
 doubleMe x = x + x
 
