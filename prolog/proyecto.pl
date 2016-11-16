c8ef0a6614f368cfcf02f0112b3c98d31fc4265a
margotduek <margotduek@gmail.com>
2016-11-16 15:45:23 -0600

    :rocket: delete rady

--- prolog/proyecto.pl
+++ prolog/proyecto.pl
@@ -9,16 +9,6 @@ leftRotation(node(APadre,PadreLeft,node(APivote,PivotLeft,PivoteRight)),node(APi
 rightRotation(node(APadre,node(APivote,PivotLeft,PivoteRight),PadreRight),node(APivote,PivotLeft,node(APadre,PadreRight,PivoteRight))).
 
 
-%Hay 4 posibilidades de detete
-% hoja -> simplemente se borra
-% Padre -> se borra y se pone el hijo en el abuelo
-%padre de 2  -> se borra se pega el hijo de la izquierda en el abuelo y otro hijo en el hermano y luego se balancea
-% root -> se le pone el más grande de la izquierda o el más chico de la derecha
-
-%delete(node(_, _, _)).
-%delete(node(X,node(Left, Lx, Ly), node(Right, Rx, Ry)), X , Out):-
-%delete(node(X, node(Left, Lx, Ly), empty), X, Out) :- Out is node(X, Lx, Ly).
-
 % First case, delete a lief
 delete(node(X, empty, empty), X, empty).
 
@@ -44,15 +34,6 @@ delete(node(X, L, node(Right, Rx, Ry)), Key, node(X, L, Out)):-
 
 
 
-%deleteb(node(APadre, node(_, Left, Right), _), node(APadre, Left, Right )).
-%delete(node(APadre, node(_, Left, Right), _), node(node(APadre, _, _), node(Left, _, _), node(Right,_, _))).
-
-
-%detele(node(_, APadre, _), node())
-%delete(node(APadre, Left, Right), )
-%delete(node(_, Left, Right), APadre, PivotLeft, node(APadre, Left, Right )).
-
-
 
 %leftRotation(node(7,node(6,empty,empty),node(9,node(8,empty,empty),node(10,empty,node(11,empty,empty)))),N).
-%delete(node(7, node( 9, node(8, empty, empty),node(10, empty, empty)), N), A).
+%delete(node(4, node(2, node(1, empty, empty), node(3, empty, empty)), node(6, node(5, empty, empty), node(8, node(7, empty, empty), node(9, empty, empty)))), 7, B).
